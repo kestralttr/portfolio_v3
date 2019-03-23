278a30f9b6278819b190bdffc3bd7fbb620bf092
Alex Bennett <kestralttr@gmail.com>
2019-03-22 21:10:12 -0600

    sets up basic environment and adds a placeholder Sidebar component

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sidebar.css';
+
+export default class Sidebar extends React.Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+    render() {
+        return(
+            <div className="Sidebar">hullo</div>
+        )
+    }
+}
