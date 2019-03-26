65d7e840153fabed64903d850a6b37c69a5b6560
Alex Bennett <kestralttr@gmail.com>
2019-03-25 20:13:04 -0600

    refactored Sidebar and ChatContainer components

---
+++ src/Components/ChatContainer/ChatContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ChatContainer = props => {
+    const children = React.Children.map(props.children, child => {
+        return(
+            <li>{props.icon} {child}</li>
+        )
+    });
+    return(
+        <div className="Sidebar-chat-container">
+        <div className="Sidebar-chat-container-header">{props.header}</div>
+        <ul>
+            {children}
+        </ul>
+    </div>
+    )
+}
+
+export default ChatContainer;
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,37 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './Sidebar.css';
+import ChatContainer from 'Components/ChatContainer/ChatContainer'
 
-export default class Sidebar extends React.Component {
-    constructor(props) {
-        super(props)
-
-
-    }
-
-    render() {
-        return(
-            <div className="Sidebar">
-                <div className="Sidebar-header">
-                    <div>Alex Bennett</div>
-                    <div>user</div>
-                </div>
-                <div className="Sidebar-chat-container">
-                    <div className="Sidebar-chat-container-header">Channels</div>
-                    <ul>
-                        <li># about</li>
-                        <li># experience</li>
-                        <li># projects</li>
-                        <li># contact</li>
-                    </ul>
-                </div>
-                <div className="Sidebar-chat-container">
-                    <div className="Sidebar-chat-container-header">Direct Messages</div>
-                    <ul>
-                        <li>> alex</li>
-                        <li>> user</li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+const Sidebar = props => {
+    return(
+        <div className="Sidebar">
+        <div className="Sidebar-header">
+            <div>Alex Bennett</div>
+            <div>user</div>
+        </div>
+        <ChatContainer icon="#" header="Channels">
+            {"about"}
+            {"experience"}
+            {"projects"}
+            {"contact"}
+        </ChatContainer>
+        <ChatContainer icon="•" header="Direct Messages">
+            {"alex"}
+            {"user"}
+        </ChatContainer>
+    </div>
+    )
 }
+
+export default Sidebar;
