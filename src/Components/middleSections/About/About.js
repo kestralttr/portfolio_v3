da8f181072ec1cdb5008b69f439fa83b13b1623b
Alex Bennett <kestralttr@gmail.com>
2019-04-24 19:25:35 -0600

    sets info on correct sections

--- src/Components/middleSections/About/About.js
+++ src/Components/middleSections/About/About.js
@@ -2,9 +2,6 @@ import React from 'react'
 import './About.css'
 import Message from 'Components/Message/Message.js'
 import Thumb_Bennett from 'Images/thumb_bennett.jpg'
-import ignshows from 'Images/ignshows.jpg'
-import petcbd from 'Images/petcbd.jpg'
-import pvpauthor from 'Images/pvpauthor.jpg'
 
 const About = props => {
     return(
@@ -12,41 +9,16 @@ const About = props => {
             <Message 
             profilePic={Thumb_Bennett} 
             authorName="alex"
-            time="11:32 AM"
-            link={{
-                text:"IGN Shows Page",
-                url:"https://www.ign.com/watch",
-                description: "IGN needed a new hub for all their shows that was mobile-friendly and easy to understand.",
-                imageSrc:ignshows
-                }}>
-                <div>I worked with IGN's designers to build this webpage for their shows.</div>
+            time="11:32 AM">
+                <div>Hey there!</div>
+                <div>Thanks for visiting my website</div>
             </Message>
             <Message 
             profilePic={Thumb_Bennett} 
             authorName="alex"
-            time="11:32 AM"
-            link={{
-                text:"IGN Author Pages",
-                url:"https://www.ign.com/watch/naomi",
-                description: "IGN author pages now pull all related videos and content from each author.",
-                imageSrc:pvpauthor
-                }}>
-                <div>The IGN author pages needed a rework.</div>
+            time="11:33 AM">
+                <div>I thought it'd be fun to lay out some of the details about myself in a Slack format because we're all so used to it.</div>
             </Message>
-
-            <Message 
-            profilePic={Thumb_Bennett} 
-            authorName="alex"
-            time="11:35 AM"
-            link={{
-                text:"Trapeze Labs Customer Sites",
-                url:"https://www.ign.com/watch/naomi",
-                description: "All customers are routed to one application, which rebuilds itself according to the current need",
-                imageSrc:petcbd
-                }}>
-                <div>All customers are directed to this conversion-optimized React application which I build from scratch.</div>
-            </Message>
-
         </div>
     );
 }
--- src/Components/middleSections/Work/Work.js
+++ src/Components/middleSections/Work/Work.js
@@ -1,10 +1,51 @@
 import React from 'react'
 import './Work.css'
+import Message from 'Components/Message/Message.js'
+import Thumb_Bennett from 'Images/thumb_bennett.jpg'
+import ignshows from 'Images/ignshows.jpg'
+import petcbd from 'Images/petcbd.jpg'
+import pvpauthor from 'Images/pvpauthor.jpg'
 
 const Work = props => {
     return(
-        <div className="section-container">
-            Work Section
+        <div className="content-container">
+            <Message 
+            profilePic={Thumb_Bennett} 
+            authorName="alex"
+            time="11:32 AM"
+            link={{
+                text:"IGN Shows Page",
+                url:"https://www.ign.com/watch",
+                description: "IGN needed a new hub for all their shows that was mobile-friendly and easy to understand.",
+                imageSrc:ignshows
+                }}>
+                <div>I worked with IGN's designers to build this webpage for their shows.</div>
+            </Message>
+            <Message 
+            profilePic={Thumb_Bennett} 
+            authorName="alex"
+            time="11:32 AM"
+            link={{
+                text:"IGN Author Pages",
+                url:"https://www.ign.com/watch/naomi",
+                description: "IGN author pages now pull all related videos and content from each author.",
+                imageSrc:pvpauthor
+                }}>
+                <div>The IGN author pages needed a rework.</div>
+            </Message>
+
+            <Message 
+            profilePic={Thumb_Bennett} 
+            authorName="alex"
+            time="11:35 AM"
+            link={{
+                text:"Trapeze Labs Customer Sites",
+                url:"https://www.ign.com/watch/naomi",
+                description: "All customers are routed to one application, which rebuilds itself according to the current need",
+                imageSrc:petcbd
+                }}>
+                <div>All customers are directed to this conversion-optimized React application which I build from scratch.</div>
+            </Message>
         </div>
     );
 }
